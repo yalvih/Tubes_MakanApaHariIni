6815f66219aa9a6707f2a1ffa17b1916be2cfe5c
yalvih <yalvi17@gmail.com>
2020-10-15 11:21:34 +0700

    test11

--- app/src/main/java/com/example/tubes_makanapahariini/FragmentListener.java
+++ app/src/main/java/com/example/tubes_makanapahariini/FragmentListener.java
@@ -4,5 +4,5 @@ public interface FragmentListener {
     void changePage(int page);
     void closeApplication();
     void showMessage(String result);
-    //YOU CAN ADD MORE INTERFACES IF THERE ANY IMPORTANCE
+    //YOU CAN ADD MORE INTERFACES IF THERE ANY IMPORTANCEs
 }
